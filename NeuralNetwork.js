2acbf7a5e03201370925a5d7d3d207c50a287524
Maxim <maxim_st@gmx.de>
2017-02-27 21:10:40 +0100

    Somewhat Working YESS

--- NeuralNetwork.js
+++ NeuralNetwork.js
@@ -1,17 +1,22 @@
 var input = [[1,0],[1,1],[0,1],[0,0]];
 var EOutput = [1,1,0,0];
 
-var synapsis = [];
-var neurons = [];
+var S = [[],[]];
+var N = [[],[]];
 
 function synapse(from, to, weight){
 	this.from = from;
 	this.to = to;
 	this.weight = weight;
 	
-	this.influence = function(input){
-		this.to.valuesToEvaluate.push(input * this.weight);
+	this.influence = function(){
+		console.log(this);
+		this.to.valuesToEvaluate.push(from.endValue * this.weight);
 	}
+}
+
+function Ineuron(input){
+	this.endValue = input;
 }
 
 function neuron(){
@@ -27,10 +32,6 @@ function neuron(){
 	}
 }
 
-function randomWeights(){
-	
-}
-
 function Sigmoid(x){
 	return 1/(1+Math.pow(Math.E, -x));
 }
@@ -39,9 +40,37 @@ function SigmoidDerivative(x){
 	return Sigmoid(x) * (1-Sigmoid(x));
 }
 
+function forwardPropagation(){
+	for(var i = 0; i < S.length; i ++){
+		for(var j = 0; j < S[i].length; j ++){
+			S[i][j].influence();
+		}
+		for(var j = 0; j < N[i].length; j ++){
+			N[i][j].getValue();
+		}
+	}
+}
+
+/*
 console.log(Sigmoid(1));
 console.log(SigmoidDerivative(1.235));
+*/
+var IOne = new Ineuron(1);
+var ITwo = new Ineuron(1);
 
-var One = new neuron();
-var SOne = new synapse(false, One, 0.4);
-var STwo = new synapse(false, One, 0.9);
+N[0][0] = new neuron();
+N[0][1] = new neuron();
+N[0][2] = new neuron();
+
+N[1][0] = new neuron();
+
+S[0][0] = new synapse(IOne, N[0][0], 0.8);
+S[0][1] = new synapse(IOne, N[0][1], 0.4);
+S[0][2] = new synapse(IOne, N[0][2], 0.3);
+S[0][3] = new synapse(ITwo, N[0][0], 0.2);
+S[0][4] = new synapse(ITwo, N[0][1], 0.9);
+S[0][5] = new synapse(ITwo, N[0][2], 0.5);
+
+S[1][0] = new synapse(N[0][0], N[1][0], 0.3);
+S[1][1] = new synapse(N[0][1], N[1][0], 0.5);
+S[1][2] = new synapse(N[0][2], N[1][0], 0.9);
